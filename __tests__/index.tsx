a04c5bb4b5be4133d8a4ca18bead9d9285f74781
Larry Botha <larry@fixate.it>
2018-06-06 08:49:39 +0200

    test(Provider and connect): add failing tests

--- __tests__/index.tsx
+++ __tests__/index.tsx
@@ -1,1 +1,23 @@
-describe('tests', () => {});
+describe('Provider', () => {
+  test('-> makes state available to components via context', () => {
+    expect(false).toBe(true);
+  });
+});
+
+describe('connect', () => {
+  test('-> wrapped component has access to state', () => {
+    expect(false).toBe(true);
+  });
+
+  test('-> wrapped component receives filtered state', () => {
+    expect(false).toBe(true);
+  });
+
+  test('-> wrapped component has access to state', () => {
+    expect(false).toBe(true);
+  });
+
+  test('-> wrapped component can dispatch actions', () => {
+    expect(false).toBe(true);
+  });
+});
